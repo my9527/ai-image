d61127ca4fd3cadd64ba75184c411dcd9219b31d
yangyang <pjt1223334444@gmail.com>
2024-10-30 20:52:55 +0800

    feat: 🎸 init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,65 @@
+"use client";
+// import Image from "next/image";
+
 import Image from "next/image";
+import { useCallback, useState } from "react";
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [_file, setFile] = useState<any>();
+    // function handleChange(e) {
+    //     console.log(e.target.files);
+    //     setFile(URL.createObjectURL(e.target.files[0]));
+    // }
+
+  const uploadFile = useCallback(async (_file: File)=>{
+
+    const formData = new FormData();
+    formData.append("file", _file);
+    fetch("http://localhost:3000/api/upload", {
+      method: "POST",
+      body: formData
+    }).then((res)=>console.log(res))
+
+  }, []);
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleFile = useCallback((e: any) => {
+    const file = e.target.files[0];
+    setFile(URL.createObjectURL(file));
+
+    uploadFile(file);
+
+
+    // const reader = new FileReader();
+    // reader.onloadend = () => {
+    //   setFile(reader.result);
+    // };
+    // reader.readAsDataURL(file);
+  }, []);
+
+
+
+  return (
+    <div className="h-screen flex justify-center items-center px-[12px] flex-col gap-10">
+      <div className={`max-w-md mx-auto border-2 border-gray-200 border-dashed p-[12px] h-[244px] w-full text-center flex justify-center items-center after:flex after:items-center after:justify-center after:w-0 after:text-gray-400 ${!_file && "after:content-['No_File_Selected'] after:w-full"}`}>
+          {
+            _file && <Image src={_file} alt="image" width={220} height={220} className="object-contain" /> 
+          }
+      </div>
+      <div className="font-[sans-serif] max-w-md mx-auto">
+        <input
+        onChange={handleFile}
+          type="file"
+          className="w-full text-gray-400 font-semibold text-sm bg-white border file:cursor-pointer cursor-pointer file:border-0 file:py-3 file:px-4 file:mr-4 file:bg-gray-100 file:hover:bg-gray-200 file:text-gray-500 rounded"
+        />
+        <p className="text-xs text-gray-400 mt-2 text-center">
+          PNG, JPG SVG, WEBP, and GIF are Allowed.
+        </p>
+      </div>
     </div>
   );
 }
